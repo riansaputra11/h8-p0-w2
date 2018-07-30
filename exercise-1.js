e7f337ff070c41c47744ed7d74f206d24a096203
riansaputra11@gmail.com <riansaputra11@gmail.com>
2018-07-30 16:51:40 +0700

    week 2 senin

---
+++ exercise-1.js
@@ -0,0 +1,48 @@
+/*nomor 1
+
+var berat = 600
+var kecepatan = 2
+var gaya = berat * kecepatan
+console.log(gaya)
+
+*/
+
+/*nomor 2
+var tahun =  1999;
+
+if (( tahun % 4 == 0 && tahun % 100 !== 0) || (tahun%400 == 0 )) {
+
+      console.log ("tahun kabisat")
+      }
+  else {
+      console.log ("bukan tahun kabisat")
+      }
+*/
+
+/*nomor 3
+var jumlahbaju = 1
+
+while (jumlahbaju <= 20) {
+     console.log("jumlah baju yang sudah dicuci" + jumlahbaju) 
+    jumlahbaju++;
+}
+*/
+
+//nomor 4
+
+var student = 1
+var kuku =  'panjang'
+
+while (student <= 40)
+{
+     console.log("siswi nomor:" + student) 
+     student++;
+     if (kuku == 'panjang')
+     {
+      console.log("hukum siswi")   
+     }
+     else
+     {
+       console.log("puji siswi")
+     }
+}
