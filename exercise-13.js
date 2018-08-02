c1266684b19e2d8a48716961a2a9d5fc9a4c3b4d
riansaputra11@gmail.com <riansaputra11@gmail.com>
2018-08-02 15:22:38 +0700

    week 2 kamis dan jumat

---
+++ exercise-13.js
@@ -0,0 +1,28 @@
+function xo(str) 
+{
+  var x = 0;
+  var o = 0;
+  for (i = 0; i < str.length; i++)
+  {
+   if(str[i] == 'x')
+   { 
+      x +=1;
+   }
+   else if (str[i] == 'o')
+   {
+      o +=1;
+   }
+  }
+  if(x == o){
+        return true;
+    }
+    else{
+        return false;
+    } 
+}
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true*/
